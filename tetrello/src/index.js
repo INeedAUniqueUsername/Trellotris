a959bd9a3ca81dfa2d3437624df5828a8e8c72eb
Alex Chen <achen115@ucr.edu>
2022-02-05 17:24:53 -0800

    work

--- tetrello/src/index.js
+++ tetrello/src/index.js
@@ -1,30 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './Trello.css'
 import reportWebVitals from './reportWebVitals';
 
 
-const width = 8
-const height = 8
+const width = 12
+const height = 10
 
 const isInBounds = (p) => p.x > -1 && p.x < width && p.y > -1 && p.y < height;
-
+const isOpen = (p, blocks) => !blocks[p.x][p.y]
 class Tetra {
   constructor(pos, tiles) {
     this.pos = pos
     this.tiles = tiles
+    this.cards = [
+      "Fix crash",
+      "Implement boring transition",
+      "Investigate mysterious bug",
+      "Refactor unnecessary feature",
+      
+    ]
   }
   getPoints = () =>
     this.tiles.map(t => ({
         x:t.x + this.pos.x,
         y:t.y + this.pos.y
       }))
+  moveUp = (blocks) => {
+    this.pos.y++;
+    for(var p of this.getPoints()){
+      if(!isInBounds(p) || !isOpen(p, blocks)){
+        this.pos.y--;
+        return false
+      }
+    }
+    return true
+  }
+  moveDown = (blocks) => {
+    this.pos.y--;
+    for(var p of this.getPoints()){
+      if(!isInBounds(p) || !isOpen(p, blocks)){
+        this.pos.y++;
+        return false
+      }
+    }
+    return true
+  }
+  moveRight = (blocks) => {
+    this.pos.x++;
+    for(var p of this.getPoints()){
+      if(!isInBounds(p) || !isOpen(p, blocks)){
+        this.pos.x--;
+        return false
+      }
+    }
+    return true
+  }
+  turnLeft = (blocks) => {
+    var next = this.tiles.map(t => ({x: -t.y, y:t.x}))
+    for(var t of next){
+      var p = {x:t.x + this.pos.x, y:t.y + this.pos.y}
+      if(!isInBounds(p) || !isOpen(p, blocks)){
+        return false
+      }
+    }
+    this.tiles = next
+    return true
+  }
+  
+  turnRight = (blocks) => {
+    var next = this.tiles.map(t => ({x: t.y, y:-t.x}))
+    for(var t of next){
+      var p = {x:t.x + this.pos.x, y:t.y + this.pos.y}
+      if(!isInBounds(p) || !isOpen(p, blocks)){
+        return false
+      }
+    }
+    this.tiles = next
+    return true
+  }
   place = (a) => {
+    let i = 0;
     for(var p of this.getPoints()){
       if(!isInBounds(p)){
         continue;
       }
-      a[p.y][p.x] = true
+      a[p.x][p.y] = this.cards[i++]
     }
   }
   fall = (a) => {
@@ -32,14 +94,14 @@ class Tetra {
       if(!isInBounds(p)){
         continue;
       }
-      if(p.y == 0){
+      if(p.x == width - 1){
         return false;
       }
-      if(a[p.y - 1][p.x]) {
+      if(a[p.x + 1][p.y]) {
         return false
       }
     }
-    this.pos.y--;
+    this.pos.x++;
     return true;
   }
 }
@@ -52,6 +114,34 @@ const L = (pos) => new Tetra(pos, [
     p(1, -1)
   ]);
 
+const I = (pos) => new Tetra(pos, [
+  p(0, 1),
+  p(0, 0),
+  p(0, -1),
+  p(0, -2)
+]);
+
+const S = (pos) => new Tetra(pos, [
+  p(0, 1),
+  p(0, 0),
+  p(-1, 0),
+  p(-1, -1)
+]);
+const O = (pos) => new Tetra(pos, [
+  p(0, 1),
+  p(0, 0),
+  p(-1, 1),
+  p(-1, 0)
+]);
+const T = (pos) => new Tetra(pos, [
+  p(0, 0),
+  p(-1, 0),
+  p(1, 0),
+  p(0, 1)
+
+])
+
+
 const MODE_IDLE = 0, MODE_FALL = 1
 class Game extends React.Component {
   constructor(props) {
@@ -61,45 +151,116 @@ class Game extends React.Component {
     this.state = {
       mode: MODE_IDLE,
       tetra: null,
-      blocks:[
-        [f, f, f, f, f, f, f, f],
-        [f, f, f, f, f, f, f, f],
-        [f, f, f, f, f, f, f, f],
-        [f, f, f, f, f, f, f, f],
-        [f, f, f, f, f, f, f, f],
-        [f, f, f, f, f, f, f, f],
-        [f, f, f, f, f, f, f, f],
-        [f, f, f, f, f, f, f, f]
-      ]
+      blocks:Array(width).fill().map((_, x) => Array(height).fill(false)),
+      moveUp: false,
+      moveDown: false,
+      moveRight:false,
+      turnLeft: false,
+      turnRight: false
     };
+
+    this.keylisten = ({ key }) => {
+      console.log(String(key))
+      if(key === "q"){
+        this.setState({turnLeft:true})
+      } else if(key === "e"){
+        this.setState({turnRight:true})
+      } else if(key === "w"){
+        this.setState({moveUp:true})
+      } else if(key === "s"){
+        this.setState({moveDown:true})
+      } else if(key === "d"){
+        this.setState({moveRight:true})
+      }
+    }
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
+  
+  componentDidMount(){
+    
+    this.updateTimer = setInterval(
+      () => this.update(),
       1000
     );
-  }
+    this.updateTurnTimer = setInterval(
+      () => this.updateTurn(),
+      250
+    );
 
+    document.addEventListener("keydown", this.keylisten, false);
+  }
   componentWillUnmount() {
-    clearInterval(this.timerID);
+    document.removeEventListener("keydown", this.keylisten, false);
+    clearInterval(this.updateTimer);
+    clearInterval(this.updateTurnTimer);
   }
-
-  tick() {
+  updateTurn(){
+    this.setState((state, props) => {
+      var { mode, tetra, blocks, moveUp, moveDown, moveRight, turnLeft, turnRight } = this.state
+      if(tetra){
+        if(moveUp){
+          tetra.moveUp(blocks)
+        }
+        if(moveDown){
+          tetra.moveDown(blocks)
+        }
+        if(moveRight){
+          tetra.moveRight(blocks)
+        }
+        if(turnLeft){
+          tetra.turnLeft(blocks)
+        }
+        if(turnRight){
+          tetra.turnRight(blocks)
+        }
+      }
+      return {
+        moveUp: false,
+        moveDown: false,
+        moveRight:false,
+        turnLeft: false,
+        turnRight: false
+      }
+    });
+  }
+  update() {
     this.setState((state, props) => {
       var { mode, tetra, blocks } = this.state
       if(tetra){
+
         if(!tetra.fall(blocks)){
           tetra.place(blocks)
           tetra = null;
         }
       } else {
-        tetra = L(p(4, 10))
+        for(let x = 0; x < width; x++){
+          var b = true
+          for(let y = 0; y < height; y++){
+            if(!blocks[x][y]) {
+              b = false
+            }
+          }
+          if(b){
+            for(let y = 0; y < height; y++){
+              blocks[x][y] = false
+            }
+            for(let x2 = x; x2 > 0; x2--){
+              for(let y = 0; y < height; y++){
+                blocks[x2][y] = blocks[x2 - 1][y]
+              }
+            }
+          }
+        }
+
+
+        var f = [L, I, S, O, T][Math.floor(Math.random() * 5)]
+
+        tetra = f(p(-1, height/2))
       }
       return {
         mode: mode,
         tetra: tetra,
-        blocks: blocks
+        blocks: blocks,
       }
     });
   }
@@ -110,20 +271,59 @@ class Game extends React.Component {
       tetra.place(apparent)
     }
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        {apparent && Array(width).fill().map((_, x) =>{
-          return <div className="column">
-            {Array(height).fill().map((_, y) => {
-              var row = apparent[height - y - 1]
-              if(row[x]){
-                return <div className="empty">TRUE</div>
-              } else {
-                return <div className="empty">.</div>
-              }
-            })}
-          </div>
-        })}
+      <div style={{margin:"0px", backgroundImage:"url(https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2048x1152/e88cf820526ba3344d163ee0521d3ae4/photo-1591989600120-ac94311508ce.jpg)"}}>
+        
+        <div dangerouslySetInnerHTML={{__html:``}}/>
+        
+        <div className="top">
+          <div className="separate"></div>
+          <h2>Trello</h2>
+          <div className="separate"></div>
+          <p>Workspaces</p>
+          <div className="separate"></div>
+          <p>Recent</p>
+          <div className="separate"></div>
+          <p>Starred</p>
+          <div className="separate"></div>
+          <p>Templates</p>
+        </div>
+        <div className="second">
+          <div className="separate"></div>
+          <p>Board</p>
+          <div className="separate"></div>
+          <p>R&amp;D</p>
+          <div className="separate"></div>
+          <p>Workspace visible</p>
+          <div className="separate"></div>
+          <p>+Invite</p>
+        </div>
+        <div style={{ overflowX:"scroll", whiteSpace:"nowrap", width:"100%"  }}>
+          {apparent && Array(width).fill().map((_, x) =>{
+            
+            return [<div className="separator"></div>, <div className="column">
+              <h4 style={{marginLeft:"24px"}}>{[
+                
+                "Backlog 5", "Backlog 4", "Backlog 3", "Backlog 2", "Backlog 1", "To do",
+                "Doing", "Waiting for PR", "Reviewed", "Done", "Deployed", "Live"
+              ][x]}</h4>
+              <div>
+
+                {Array(height).fill().map((_, y) => {
+                  var col = apparent[x]
+                  var b = col[height - y - 1] 
+                  if(b){
+                    return <div className="card"><p>{b}</p></div>
+                  } else {
+                    return <pre className="empty"> </pre>
+                  }
+                })}
+                
+                <pre className="empty"> </pre>
+              </div>
+            </div>]
+          })}
+          <div className="separator"></div>
+        </div>
       </div>
     );
   }
